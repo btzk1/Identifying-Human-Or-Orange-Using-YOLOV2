698b93759bcc232a367e4687959d630757e00f7f
btzk1 <62603516+btzk1@users.noreply.github.com>
2021-07-30 14:10:20 +0800

    Add files via upload

---
+++ Project_Iterator.java
@@ -0,0 +1,73 @@
+package ai.certifai.Group_Project;
+
+
+import ai.certifai.Helper;
+import ai.certifai.solution.object_detection.AvocadoBananaDetector.FruitDataSetIterator;
+import ai.certifai.utilities.VocLabelProvider;
+import org.apache.commons.io.FileUtils;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.split.InputSplit;
+import org.datavec.image.loader.NativeImageLoader;
+import org.datavec.image.recordreader.objdetect.ObjectDetectionRecordReader;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.nd4j.common.util.ArchiveUtils;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.slf4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+
+public class Project_Iterator {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(Project_Iterator.class);
+    private static final int seed = 123;
+    private static Random rng = new Random(seed);
+    private static String dataDir;
+    private static Path trainDir, testDir;
+    private static FileSplit trainData, testData;
+    private static final int nChannels = 3;
+    public static final int gridWidth = 13;
+    public static final int gridHeight = 13;
+    public static final int yolowidth = 416;
+    public static final int yoloheight = 416;
+
+    private static RecordReaderDataSetIterator makeIterator(InputSplit split, Path dir, int batchSize) throws Exception {
+
+        ObjectDetectionRecordReader recordReader = new ObjectDetectionRecordReader(yoloheight, yolowidth, nChannels,
+                gridHeight, gridWidth, new VocLabelProvider(dir.toString()));
+        recordReader.initialize(split);
+        RecordReaderDataSetIterator iter = new RecordReaderDataSetIterator(recordReader, batchSize, 1, 1, true);
+        iter.setPreProcessor(new ImagePreProcessingScaler(0, 1));
+
+        return iter;
+    }
+
+    public static RecordReaderDataSetIterator trainIterator(int batchSize) throws Exception {
+        return makeIterator(trainData, trainDir, batchSize);
+    }
+
+    public static RecordReaderDataSetIterator testIterator(int batchSize) throws Exception {
+        return makeIterator(testData, testDir, batchSize);
+    }
+
+    public static void setup() throws IOException {
+        log.info("Load data...");
+        loadData();
+        trainDir = Paths.get(dataDir, "Orange", "train");
+        testDir = Paths.get(dataDir, "Orange", "test");
+        trainData = new FileSplit(new File(trainDir.toString()), NativeImageLoader.ALLOWED_FORMATS, rng);
+        testData = new FileSplit(new File(testDir.toString()), NativeImageLoader.ALLOWED_FORMATS, rng);
+    }
+
+    private static void loadData() throws IOException {
+        dataDir = Paths.get(
+                System.getProperty("user.home"),
+                Helper.getPropValues("dl4j_home.data")
+        ).toString();
+
+    }
+
+}
